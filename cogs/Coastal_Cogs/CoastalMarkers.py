cc63dd74f5eb75ea4f84af5d63febfa7f3d57956
BSavage81 <beau.j.savage@gmail.com>
2022-02-13 00:36:36 -0600

    Update CoastalMarkers.py

--- cogs/Coastal_Cogs/CoastalMarkers.py
+++ cogs/Coastal_Cogs/CoastalMarkers.py
@@ -114,11 +114,6 @@ def convert(time):
         return time
 
 
-class CoastalMarkersCMD(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        logger.info("RealmCMD: Cog Loaded!")
-
 class MyModal(Modal):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
@@ -138,60 +133,64 @@ class MyModal(Modal):
         embed.add_field(name="Second Input", value=self.children[1].value, inline=False)
         await interaction.response.send_message(embeds=[embed])
 
+class CoastalMarkersCMD(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        logger.info("RealmCMD: Cog Loaded!")
 
-@commands.slash_command(name="modaltest", guild_ids=[config['PBtest']])
-async def modal_slash(ctx):
-    """Shows an example of a modal dialog being invoked from a slash command."""
-    modal = MyModal(title="Slash Command Modal")
-    await ctx.interaction.response.send_modal(modal)
-
-
-@commands.message_command(name="messagemodal", guild_ids=[config['PBtest']])
-async def modal_message(ctx, message):
-    """Shows an example of a modal dialog being invoked from a message command."""
-    modal = MyModal(title="Message Command Modal")
-    modal.title = f"Modal for Message ID: {message.id}"
-    await ctx.interaction.response.send_modal(modal)
+    @commands.slash_command(name="modaltest", guild_ids=[config['PBtest']])
+    async def modal_slash(ctx):
+        """Shows an example of a modal dialog being invoked from a slash command."""
+        modal = MyModal(title="Slash Command Modal")
+        await ctx.interaction.response.send_modal(modal)
 
 
-@commands.user_command(name="usermodal", guild_ids=[config['PBtest']])
-async def modal_user(ctx, member):
-    """Shows an example of a modal dialog being invoked from a user command."""
-    modal = MyModal(title="User Command Modal")
-    modal.title = f"Modal for User: {member.display_name}"
-    await ctx.interaction.response.send_modal(modal)
+    @commands.message_command(name="messagemodal", guild_ids=[config['PBtest']])
+    async def modal_message(ctx, message):
+        """Shows an example of a modal dialog being invoked from a message command."""
+        modal = MyModal(title="Message Command Modal")
+        modal.title = f"Modal for Message ID: {message.id}"
+        await ctx.interaction.response.send_modal(modal)
 
 
-@commands.command()
-async def modaltest(ctx):
-    """Shows an example of modals being invoked from an interaction component (e.g. a button or select menu)"""
+    @commands.user_command(name="usermodal", guild_ids=[config['PBtest']])
+    async def modal_user(ctx, member):
+        """Shows an example of a modal dialog being invoked from a user command."""
+        modal = MyModal(title="User Command Modal")
+        modal.title = f"Modal for User: {member.display_name}"
+        await ctx.interaction.response.send_modal(modal)
 
-    class MyView(discord.ui.View):
-        @discord.ui.button(label="Modal Test", style=discord.ButtonStyle.primary)
-        async def button_callback(self, button, interaction):
-            modal = MyModal(title="Modal Triggered from Button")
-            await interaction.response.send_modal(modal)
 
-        @discord.ui.select(
-            placeholder="Pick Your Modal",
-            min_values=1,
-            max_values=1,
-            options=[
-                discord.SelectOption(
-                    label="First Modal", description="Shows the first modal"
-                ),
-                discord.SelectOption(
-                    label="Second Modal", description="Shows the second modal"
-                ),
-            ],
-        )
-        async def select_callback(self, select, interaction):
-            modal = MyModal(title="Temporary Title")
-            modal.title = select.values[0]
-            await interaction.response.send_modal(modal)
+    @commands.command()
+    async def modaltest(ctx):
+        """Shows an example of modals being invoked from an interaction component (e.g. a button or select menu)"""
 
-    view = MyView()
-    await ctx.send("Click Button, Receive Modal", view=view)
+        class MyView(discord.ui.View):
+            @discord.ui.button(label="Modal Test", style=discord.ButtonStyle.primary)
+            async def button_callback(self, button, interaction):
+                modal = MyModal(title="Modal Triggered from Button")
+                await interaction.response.send_modal(modal)
+
+            @discord.ui.select(
+                placeholder="Pick Your Modal",
+                min_values=1,
+                max_values=1,
+                options=[
+                    discord.SelectOption(
+                        label="First Modal", description="Shows the first modal"
+                    ),
+                    discord.SelectOption(
+                        label="Second Modal", description="Shows the second modal"
+                    ),
+                ],
+            )
+            async def select_callback(self, select, interaction):
+                modal = MyModal(title="Temporary Title")
+                modal.title = select.values[0]
+                await interaction.response.send_modal(modal)
+
+        view = MyView()
+        await ctx.send("Click Button, Receive Modal", view=view)
 
 def setup(bot):
     bot.add_cog(CoastalMarkersCMD(bot))
