76b2925b8447194e69f4661c4a8a942cad720b25
BSavage81 <beau.j.savage@gmail.com>
2023-08-11 20:24:36 -0500

    update database

--- core/database.py
+++ core/database.py
@@ -80,78 +80,14 @@ class BotData(BaseModel):
     Server ID
     """
     id = AutoField()
-    last_question_posted = TextField(null=True)
     persistent_views = BooleanField(default=False)
     prefix = TextField(default=">")
-    blacklist_response_channel = BigIntegerField(default=0)
-    daily_question_channel = BigIntegerField(default=0)
-    question_suggest_channel = BigIntegerField(default=0)
-    bot_spam_channel = BigIntegerField(default=0)
-    realm_channel_response = BigIntegerField(default=0)
+    bot_spam_channel_coastal = BigIntegerField(default=0)
+    bot_spam_channel_aurafall = BigIntegerField(default=0)
     bot_type = TextField(default="Stable")
-    other_bot_id = BigIntegerField(default=0)
     bot_id = BigIntegerField(default=0)
-    server_id = BigIntegerField(default=0)
-
-
-
-class Tag(BaseModel):
-    """Stores our tags accessed by the tag command."""
-    id = AutoField()
-    tag_name = TextField()
-    embed_title = TextField()
-    text = TextField()
-
-
-class Question(BaseModel):
-    """Stores Questions for DailyQ here"""
-    id = AutoField()
-    question = TextField()
-    usage = TextField(default=False)
-
-
-class MRP_Blacklist_Data(BaseModel):
-    """Stores Blacklist Data here"""
-    entryid = AutoField()
-    BanReporter = TextField()
-    DiscUsername = TextField()
-    DiscID = TextField()
-    Gamertag = TextField()
-    BannedFrom = TextField()
-    KnownAlts = TextField()
-    ReasonforBan = TextField()
-    DateofIncident = TextField()
-    TypeofBan = TextField()
-    DatetheBanEnds = TextField()
-
-
-class PortalbotProfile(BaseModel):
-    """Stores Profile Data here"""
-    entryid = AutoField()
-    DiscordName = TextField()
-    DiscordLongID = TextField()
-    Timezone = TextField(default="None")
-    XBOX = TextField(default="None")
-    Playstation = TextField(default="None")
-    Switch = TextField(default="None")
-    PokemonGo = TextField(default="None")
-    Chessdotcom = TextField(default="None")
-
-
-class RealmProfile(BaseModel):
-    """Stores Realm Profile Data here"""
-    entryid = AutoField()
-    RealmName = TextField()
-    RealmEmoji = TextField()
-    RealmLongDesc = TextField()
-    RealmShortDesc = TextField()
-    Realmaddons = TextField()
-    WorldAge = TextField()
-    PVP = TextField()
-    OnePlayerSleep = TextField()
-    RealmStyle = TextField()
-    Gamemode = TextField()
-
+    server_id_aurafall = BigIntegerField(default=0)
+    server_id_coastal = BigIntegerField(default=0)
 
 class Administrators(BaseModel):
     """
@@ -177,96 +113,6 @@ class Administrators(BaseModel):
     discordID = BigIntegerField(unique=True)
     TierLevel = IntegerField(default=1)
 
-
-class QuestionSuggestionQueue(BaseModel):
-    """
-    QuestionSuggestionQueue:
-    List of users who have suggested questions for the bot.
-
-    `id`: AutoField()
-    Database Entry
-
-    `discord_id`: BigIntegerField()
-    Discord ID
-
-    `question`: TextField()
-    Question
-
-    `message_id`: BigIntegerField()
-    Message ID
-    """
-
-    id = AutoField()
-    discord_id = BigIntegerField()
-    question = TextField()
-    message_id = BigIntegerField()
-
-
-class RealmApplications(BaseModel):
-    """
-    RealmApplications:
-    List of users who have applied for a realm and their application details.
-
-    `id`: AutoField()
-    Database Entry
-
-    `discord_id`: BigIntegerField()
-    Discord ID
-
-    `realm_name`: TextField()
-    Realm Name
-
-    `type_of_realm`: TextField()
-    Realm Type
-
-    `emoji`: TextField()
-    Realm Emoji
-
-    `short_desc`: TextField()
-    Realm Short Description
-
-    `long_desc`: TextField()
-    Realm Long Description
-
-    `application_process`: TextField()
-    Realm Application Process
-
-    `member_count`: IntegerField()
-    Realm Member Count
-
-    `realm_age`: TextField()
-    Realm Age
-
-    `world_age`: TextField()
-    Realm World Age
-
-    `reset_schedule`: TextField()
-    Realm Reset Schedule
-
-    `foreseeable_future`: TextField()
-    Realm Foreseeable Future
-
-    `admin_team`: TextField()
-    Realm Admin Team
-    """
-    id = AutoField()
-    discord_id = BigIntegerField()
-    discord_name = TextField()
-    realm_name = TextField()
-    type_of_realm = TextField()
-    emoji = TextField()
-    short_desc = TextField()
-    long_desc = TextField()
-    application_process = TextField()
-    member_count = TextField()
-    realm_age = TextField()
-    world_age = TextField()
-    reset_schedule = TextField()
-    foreseeable_future = TextField()
-    admin_team = TextField()
-    timestamp = TimestampField()
-
-
 app = Flask(__name__)
 
 
@@ -286,14 +132,7 @@ def _db_close(exc):
 
 
 tables = {
-    "tag": Tag,
-    "questions": Question,
-    "blacklist": MRP_Blacklist_Data,
-    "profile": PortalbotProfile,
-    "realmprofile": RealmProfile,
     "administrators": Administrators,
-    "questionsuggestionqueue": QuestionSuggestionQueue,
-    "realmapplications": RealmApplications,
     "botdata": BotData
 }
 iter_table(tables)
