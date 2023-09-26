c6380843f40a344c9cf72a35c737fa13fb0e4704
BSavage81 <beau.j.savage@gmail.com>
2023-09-26 11:52:20 -0500

    Update repeat.py

--- utils/Global_cogs/repeat.py
+++ utils/Global_cogs/repeat.py
@@ -6,11 +6,10 @@ class RepeatCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command()
+    @app_commands.command(name='repeat', description='Repeats what you type')
     @app_commands.describe(
         message="Phrase to repeat"
-    )
-    @commands.command(name='repeat', description='Repeats what you type')
+    )    
     async def repeat(self, ctx, interaction: discord.Interaction, *, message: str):
         await ctx.send(message)
 
