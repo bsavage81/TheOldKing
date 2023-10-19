b33b65ce3a7d70fc68f6e684a82c0757fb51ae7d
BSavage81 <beau.j.savage@gmail.com>
2023-10-19 12:42:30 -0500

    Update CoastalAppCMD.py

--- utils/Coastal_cogs/CoastalAppCMD.py
+++ utils/Coastal_cogs/CoastalAppCMD.py
@@ -132,7 +132,9 @@ class CoastalAppCMD(commands.Cog):
         channel2 = interaction.channel
         author = interaction.user.id
         channel = await interaction.user.create_dm()
-        responseguild = self.bot.get_guild(config['Coastal'])
+        print(config["Coastal"])
+        responseguild = self.bot.get_guild(config["Coastal"])
+        print(responseguild)
         responseChannel = responseguild.get_channel(
             config['CoastalApplications'])
         admin = responseguild.get_role(config['CoastalOPTeam'])
