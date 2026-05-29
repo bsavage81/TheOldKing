948de0f2e0212fd49049186465565ff48864caf3
BSavage81 <beau.j.savage@gmail.com>
2026-05-29 18:41:49 -0500

    update

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import time
 from pathlib import Path
 
 import discord
-import xbox
 from alive_progress import alive_bar
 from discord import app_commands
 from discord.ext import commands
@@ -25,8 +24,14 @@ from sentry_sdk.integrations.logging import LoggingIntegration
 from core import database
 from core.common import get_bot_data_id
 from core.logging_module import get_log
-from core.special_methods import on_app_command_error_, initialize_db, on_ready_, on_command_error_, on_command_, \
-    before_invoke_
+from core.special_methods import (
+    on_app_command_error_,
+    initialize_db,
+    on_ready_,
+    on_command_error_,
+    on_command_,
+    before_invoke_,
+)
 
 logger = logging.getLogger("discord")
 logger.setLevel(logging.INFO)
@@ -36,16 +41,9 @@ _log.info("Starting TheOldKing...")
 load_dotenv()
 
 row_id = get_bot_data_id()
-bot_info: database.BotData = database.BotData.select().where(
-    database.BotData.id == row_id).get()
-
-try:
-    xbox.client.authenticate(
-        login=os.getenv('xbox_u'),
-        password=os.getenv('xbox_p'),
-    )
-except:
-    logger.critical("ERROR: Unable to authenticate with XBOX!")
+bot_info: database.BotData = (
+    database.BotData.select().where(database.BotData.id == row_id).get()
+)
 
 
 def get_extensions():  # Gets extension list dynamically
@@ -63,10 +61,11 @@ class OKCommandTree(app_commands.CommandTree):
         self.bot = bot
 
     async def interaction_check(self, interaction: discord.Interaction, /) -> bool:
-        #blacklisted_users = [p.discordID for p in database.Blacklist]
+        # blacklisted_users = [p.discordID for p in database.Blacklist]
         if interaction.user.avatar is None:
             await interaction.response.send_message(
-                "Due to a discord limitation, you must have an avatar set to use this command.")
+                "Due to a discord limitation, you must have an avatar set to use this command."
+            )
             return False
         """if interaction.user.id in blacklisted_users:
             await interaction.response.send_message(
@@ -76,7 +75,7 @@ class OKCommandTree(app_commands.CommandTree):
         return True
 
     async def on_error(
-            self, interaction: discord.Interaction, error: app_commands.AppCommandError
+        self, interaction: discord.Interaction, error: app_commands.AppCommandError
     ):
         await on_app_command_error_(self.bot, interaction, error)
 
@@ -95,10 +94,11 @@ class TheOldKing(commands.Bot):
             status=discord.Status.online,
             activity=discord.Activity(
                 type=discord.ActivityType.watching,
-                name=f"over the Portal! | {bot_info.prefix}help")
+                name=f"over the Portal! | {bot_info.prefix}help",
+            ),
         )
         self.help_command = None
-        #self.add_check(self.check)
+        # self.add_check(self.check)
         self._start_time = uptime
 
     async def on_ready(self):
@@ -115,10 +115,10 @@ class TheOldKing(commands.Bot):
 
     async def setup_hook(self) -> None:
         with alive_bar(
-                len(get_extensions()),
-                ctrl_c=False,
-                bar="bubbles",
-                title="Initializing Cogs:",
+            len(get_extensions()),
+            ctrl_c=False,
+            bar="bubbles",
+            title="Initializing Cogs:",
         ) as bar:
 
             for ext in get_extensions():
@@ -134,7 +134,8 @@ class TheOldKing(commands.Bot):
 
     async def is_owner(self, user: discord.User):
         query = database.Administrators.select().where(
-            (database.Administrators.TierLevel >= 3) & (database.Administrators.discordID == user.id)
+            (database.Administrators.TierLevel >= 3)
+            & (database.Administrators.discordID == user.id)
         )
         if query.exists():
             return True
@@ -152,7 +153,9 @@ class TheOldKing(commands.Bot):
 
         version = ...  # type: str
         if current_branch == "HEAD":
-            current_tag = repo.describe(committish=current_commit, describe_strategy=GIT_DESCRIBE_TAGS)
+            current_tag = repo.describe(
+                committish=current_commit, describe_strategy=GIT_DESCRIBE_TAGS
+            )
             version = f"{current_tag} (stable)"
         else:
             version = "development"
@@ -177,7 +180,7 @@ class TheOldKing(commands.Bot):
 
 bot = TheOldKing(time.time())
 
-if os.getenv('sentry_dsn') is not None:
+if os.getenv("sentry_dsn") is not None:
     sentry_logging = LoggingIntegration(
         level=logging.INFO,  # Capture info and above as breadcrumbs
         event_level=logging.ERROR,  # Send errors as events
@@ -186,7 +189,7 @@ if os.getenv('sentry_dsn') is not None:
     # Traceback tracking, DO NOT MODIFY THIS
     use_sentry(
         bot,
-        dsn=os.getenv('sentry_dsn'),
+        dsn=os.getenv("sentry_dsn"),
         traces_sample_rate=1.0,
         integrations=[FlaskIntegration(), sentry_logging],
     )
@@ -194,8 +197,8 @@ if os.getenv('sentry_dsn') is not None:
 initialize_db(bot)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        bot.run(os.getenv('token'))
+        bot.run(os.getenv("token"))
     except Exception as e:
         _log.exception(e)
